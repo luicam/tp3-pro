202ec5c664f3104093c4178040aeceaa27553e9a
luicam <35222599+luicam@users.noreply.github.com>
2018-11-14 17:38:16 +0100

    Add files via upload

---
+++ mayorEdadAlumnos.py
@@ -0,0 +1,18 @@
+from sys import argv
+#datos del fichero: nombre#edad#curso#genero#otrosDatos(separador ",")
+
+from misfunciones import comprobarEdad
+
+fe = open(argv[1], "r")
+fs = open(argv[2], "w")
+linea = fe.readline().strip()
+while linea != "":
+  palabras = linea.split("#")
+  if palabras[1] > 18:
+  	edad=comprobarEdad(palabras[0])
+  	print(palabras[0]+" no estara en el fichero de salida por que tiene "+str(edad)+" anyos.")
+  	fs.write(line)
+  linea = fe.readline().strip()
+fe.close()
+
+
---
+++ misfunciones.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+
+def comprobarEdad(nombre):
+  with open(argv[1], "r") as f:
+      for line in f:  
+        campos = line.strip().split(':')
+        if campos[0] == nombre:
+          return campos[1]
