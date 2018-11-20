8c112cd04c71b35a3ba7551883481b0e50107232
luicam <35222599+luicam@users.noreply.github.com>
2018-11-20 13:49:34 +0100

    Update misfunciones.py

--- misfunciones.py
+++ misfunciones.py
@@ -4,6 +4,6 @@ from sys import argv
 def comprobarEdad(nombre):
   with open(argv[1], "r") as f:
       for line in f:  
-        campos = line.strip().split(':')
+        campos = line.strip().split('#')
         if campos[0] == nombre:
           return campos[1]
